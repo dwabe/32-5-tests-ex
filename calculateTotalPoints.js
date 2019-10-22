bfd22c10dfc43cc6b961b26aeee93a7a82050bd5
Bartek Błęcki <bartek@blecki.pl>
2019-10-22 17:52:09 +0200

    update calculateTotalPoints.js

--- calculateTotalPoints.js
+++ calculateTotalPoints.js
@@ -1,11 +1,29 @@
 const calculateDistancePoints = require('./calculateDistancePoints');
 const calculateStylePoints = require('./calculateStylePoints');
 
-const calculateTotalPoints = (distance, hillSize, kPoint, styleNotes, windFactor, gateFactor) => {
+const calculateTotalPoints = (distance, hillSize, styleNotes, kPoint, windFactor, gateFactor, ) => {
+  if (typeof windFactor != 'number') {
+    windFactor = Number(windFactor);
+
+    if (isNaN(windFactor) == true) {
+      windFactor = 0;
+    };
+  };
+
+  if (typeof gateFactor != 'number') {
+    gateFactor = Number(gateFactor);
+
+    if (isNaN(gateFactor) == true) {
+      gateFactor = 0;
+    };
+  };
+
   const distancePoints = calculateDistancePoints(distance, hillSize, kPoint);
   const stylePoints = calculateStylePoints(styleNotes);
+  const totalPoints = distancePoints + stylePoints + windFactor + gateFactor;
 
-  return distancePoints + stylePoints + windFactor + gateFactor;
-}
+  console.log(totalPoints);
+  return totalPoints
+};
 
 module.exports = calculateTotalPoints;
